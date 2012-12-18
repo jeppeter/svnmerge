765c863246d2c16ff330c0c5e8106246977883aa
jeppeter <jeppeter@gmail.com>
2012-12-18 16:18:43 +0800

    1, to make origin dircmp.pl to ldircmp.pl and to make the use dirtime.pl into rdircmp.pl

---
+++ ldircmp.pl
@@ -0,0 +1,68 @@
+#! perl -w
+
+use Getopt::Std;
+use vars qw ($opt_h $opt_s $opt_d $opt_v);
+
+sub Usage($)
+{
+	my ($ec) = @_;
+	my ($fp);
+	$fp = STDERR;
+	if ($ec == 0)
+	{
+		$fp = STDOUT;
+	}
+	print $fp "dircmp [OPTIONS] \@filters\n";
+	print $fp "-d dest\n";
+	print $fp "-s src\n";
+
+	exit($ec);
+}
+sub DebugString($)
+{
+    my ($str)=@_;
+    my ($package,$file,$line,$func)=caller(0);
+
+    print STDERR "[$file][$func]:$line $str";
+}
+
+getopts("hs:d:v");
+
+if (!defined($opt_s) || !defined($opt_d))
+{
+	Usage(3);
+}
+
+if (defined($opt_h))
+{
+	Usage(0);
+}
+
+my (@filters) = $#ARGV >= 0 ?  @ARGV : ();
+my ($cmd,$ret);
+
+$cmd = " perl dirsha.pl \"$opt_d\" ";
+
+if ( $#filters >= 0 )
+{
+	$cmd .= join(" ",@filters);
+}
+
+if (defined($opt_v))
+{
+	$cmd .= " | perl dirsha.pl -v -f - \"$opt_s\" ";
+}
+else
+{
+	$cmd .= " | perl dirsha.pl -f - \"$opt_s\" ";
+}
+
+if ( $#filters >= 0)
+{
+	$cmd .= join(" ",@filters);
+}
+
+$ret = system($cmd);
+DebugString("run cmd $cmd ".($ret == 0 ? "Succ" : "Failed $ret")."\n");
+
+
